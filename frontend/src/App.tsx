c41b280d5a9da95196d12e8cfa358d557eb447f5
Renato Gouveia <rpgouveia@gmail.com>
2026-03-21 20:18:47 -0300

    feat: update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BookList from "./pages/BookList";
+import BookForm from "./pages/BookForm";
+import BookDetails from "./pages/BookDetails";
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <h1>RPG Shelf</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/books/new" element={<BookForm />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/books/:id/edit" element={<BookForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
