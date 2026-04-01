4a61be57001c9eb314f6a4b66911a6328c5dc14b
Renato Gouveia <rpgouveia@gmail.com>
2026-04-01 12:43:18 -0300

    chore: rename index.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import "normalize.css";
-import "./styles/index.css";
+import "./styles/Index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
