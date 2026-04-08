7059b43c50a8f25e106f86d724cd6754ff3e5426
Renato Gouveia <rpgouveia@gmail.com>
2026-04-08 11:13:43 -0300

    fix: update book type in frontend

--- frontend/src/types/book.ts
+++ frontend/src/types/book.ts
@@ -2,9 +2,9 @@ export interface Book {
     id?: number;
     title: string;
     system: string;
-    publisher: string;
+    publisher?: string;
     author?: string;
-    edition?: string;
+    edition: string;
     status?: "Quero" | "Tenho" | "Lendo" | "Lido";
     notes?: string;
     created_at?: string;
